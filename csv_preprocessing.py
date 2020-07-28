82e5729fe3d16a33ea14e70aee3fc2c42452744f
arnav-verma22 <arn.verma22@gmail.com>
2020-07-28 19:20:34 +0530

    reading csv file

--- csv_preprocessing.py
+++ csv_preprocessing.py
@@ -4,5 +4,6 @@ import pandas as pd
 import csv
 dataset = pd.read_csv("CF-Insider-Trading-equities-28-04-2020-to-28-07-2020.csv")
 X = dataset.iloc[:, :].values
-
+groups = ['Promoters', 'Promoter Group']
+dataset = dataset[dataset['category of person'].isin(groups)]
 
