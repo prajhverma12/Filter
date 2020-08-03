9acfb0ee93589d2aa1f6b9c6363f685c7f60a8bb
prajwal <prajh.kool@gmail.com>
2020-08-04 05:03:46 +0530

    added print logger

--- fileutility.py
+++ fileutility.py
@@ -1,18 +1,17 @@
 import os
-import logging
 
 def checkfile(filename):
     arr = os.listdir('.')
     #print(arr)
     if filename in arr:
-        logging.info("File {} present in the directory".format(filename))
+        print("File {} present in the directory".format(filename))
         return True
     else:
-        logging.info("File {} not present in the directory".format(filename))
+        print("File {} not present in the directory".format(filename))
         return False
     
 def deletefile(filename):
     if checkfile(filename):
         os.remove(filename)
     else:
-        logging.info("File {} not present in the directory".format(filename))
+        print("File {} not present in the directory".format(filename))
